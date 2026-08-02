ca212e4ffc2d3383bf930dd3f4e3b3f203119815
Wagner <WagnerExpansiva@users.noreply.github.com>
2026-08-02 20:50:59 -0300

    clean

--- l2/dteste.ts
+++
@@ -1,3 +0,0 @@
-/// <mls fileReference="_102046_/l2/dteste.ts" enhancement="_blank"/> 
-
-export const b = 5; 
--- l2/eteste.ts
+++
@@ -1,4 +0,0 @@
-/// <mls fileReference="_102046_/l2/eteste.ts" enhancement="_blank"/>
-
-console.info('c') 
- 
